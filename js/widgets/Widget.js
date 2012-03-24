92c716e23f8600301cf0fdda6b629798c9c71319
Vitaliy Stepanenko <mail@vitaliy.in>
2012-03-24 17:28:59 +0200

    Widget class refactored

--- js/widgets/Widget.js
+++ js/widgets/Widget.js
@@ -1,21 +1,57 @@
-define(['jquery','jquery.tmpl', 'Class'], function ($) {
-    var defaults = {
-        template:'',
-        insertTo:'body',
-        data:{}
-    }
+/**
+ * Widget
+ */
+define(['jquery', 'jquery.tmpl', 'Class'], function ($) {
+    /**
+     * This class have asynchronous constructor
+     * (it can return defferred object instead of widget instance)
+     *
+     * Usage:
+     * <code>
+     * var myWidget;
+     * $.when(new Widget(...)).done(function(obj){
+     *     myWidget = obj;
+     * })
+     * </code>
+     *
+     * required options:
+     *   one of:
+     *      tplSrc -- url of template file
+     *      tplBody -- template body (html)
+     * other supported options:
+     *      placeTo -- selector of DOM element that will contain widget
+     *
+     *
+     * @param config
+     */
     Widget = function (config) {
         var me = this;
-        var options = merge({}, config);
-        mergeUndefined(options, defaults);
-        me.loading = $.get(options.template, {}, function (templateBody) {
-                me.$el = $.tmpl(templateBody, options.data).appendTo(options.insertTo);
-        });
+        if (config.tplSrc) {
+            return $.get(config.tplSrc, {}, function (templateBody) {
+                var options = merge({},config);
+                options.tplBody = templateBody;
+                me.tplSrc = options.tplSrc;
+                delete options.tplSrc;
+                return Widget.call(me, options);
+            });
+        } else if (config.tplBody) {
+            me.tplBody = config.tplBody;
+            me.$el = $.tmpl(config.tplBody, config.data ? config.data : {});
+        } else {
+            throw new Error('Widget template not specified: required tplSrc or tplBody properties.');
+        }
+
+        if (config.placeTo) {
+            me.placeTo(config.placeTo);
+        }
+
+        return me;
     }
 
     Widget.prototype = {
-
-
+        placeTo:function (targetSelector) {
+            $(this.$el).appendTo(targetSelector);
+        }
     }
     return Widget;
 });
--- js/widgets/sidebar/sidebar.js
+++ js/widgets/sidebar/sidebar.js
@@ -1,6 +1,9 @@
 define(['widgets/Widget'], function (Widget) {
 	
     // @todo Firefox already have global object sidebar
-    var sidebar = new Widget({template:'/js/widgets/sidebar/sidebar.tpl'});	
+    var sidebar = new Widget({
+        tplSrc:'/js/widgets/sidebar/sidebar.tpl',
+        placeTo:'body'
+    });
     return sidebar;
 });

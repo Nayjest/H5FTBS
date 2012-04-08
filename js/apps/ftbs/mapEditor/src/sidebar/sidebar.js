22ab3125451061b2fdf676689301625de4f25d78
Vitaliy Stepanenko <mail@vitaliy.in>
2012-04-09 00:52:35 +0300

    mapEditor: old widgets removed

--- js/apps/ftbs/mapEditor/src/sidebar/sidebar.js
+++
@@ -1,9 +0,0 @@
-define(['widgets/Widget'], function (Widget) {
-	
-    // @todo Firefox already have global object sidebar
-    var sidebar = new Widget({
-        tplSrc:'/js/apps/ftbs/mapEditor/src/sidebar/sidebar.tpl',
-        placeTo:'body'
-    });
-    return sidebar;
-});

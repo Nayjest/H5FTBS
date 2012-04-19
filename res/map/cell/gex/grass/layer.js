89fce996ddf05c680f93111d0bb01a1e1befb56a
Vitaliy Stepanenko <mail@vitaliy.in>
2012-04-20 00:42:08 +0300

    fixed bug with undefined variables in resources

--- res/map/cell/gex/grass/layer.js
+++ res/map/cell/gex/grass/layer.js
@@ -1,6 +1,6 @@
 res={
     image: '/res/map/cell/gex/grass/img/set1/' + Utils.rndInt(1,6) + '.png',
-    zIndex:Utils.rndInt(Map.zLevels.ground),
+    zIndex:require('utils').rndInt(require('map/Map').zLevels.ground),
     size:[105, 105]
 }
 //some code can beplaced here

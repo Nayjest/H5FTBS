0d636f87b57840ce2b2e3f8be05ab998bc1c8aec
Vitaliy Stepanenko <mail@vitaliy.in>
2012-04-19 23:13:10 +0300

    mapEditor: working on objects & units export

--- js/map/export/mapExport.js
+++ js/map/export/mapExport.js
@@ -1,12 +1,22 @@
+/**
+ * Returns serialized map, you can save it to file and then load using following code:
+ * <code>
+ *     var map;
+ *     Map.load('map_file_name', function (m) {
+ *          map = m;
+ *     }
+ * </code>
+ */
 define([], function () {
     "use strict";
+    // @todo it's not used now
     function wrapToModule(source) {
         return 'define([],function(){var export =  ' + source + '; return export;});';
     }
 
     var me = {
         export:function (map) {
-            var json = {},
+            var json = {units:[], objects:[]},
                 cells = [];
             for (var x = map.cells.length; x--;) {
                 cells[x] = [];
@@ -16,6 +26,14 @@ define([], function () {
             }
             json.size = [cells.length, cells[0].length];
             json.cells = cells;
+            for (var i = map.units.length; i--;) {
+                if (map.units[i].loadedFrom)
+                    json.units.push(map.units[i].loadedFrom)
+            }
+            for (var i = map.objects.length; i--;) {
+                if (map.objects[i].loadedFrom)
+                    json.objects.push(map.objects[i].loadedFrom)
+            }
 
             var strMap = JSON.stringify(json);
             //strMap = wrapToModule(strMap);
